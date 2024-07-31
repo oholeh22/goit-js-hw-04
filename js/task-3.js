ca3c25bab96db5d0efd2d84d4721249d47c7d054
Oleh Shyian <shiyan4242@gmail.com>
2024-07-31 17:00:23 +0200

    all tasks

---
+++ js/task-3.js
@@ -0,0 +1,31 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+
+    newName(username, playTime = 0) {
+        this.username = username;
+        this.playTime = playTime;
+    },
+
+    changeUsername(newName) {
+        this.username = newName;
+    },
+
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    }
+};
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
